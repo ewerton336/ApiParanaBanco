d42940b14c4d1cb060cbf8cceb14f1e77209b5db
Ewerton  Guimarães <ewertonpg@hotmail.com>
2022-07-25 15:36:54 -0300

    inclusão de comentários dentro do controller para ser exibido no xml e swagger

--- ApiParanaBanco/ApiParanaBanco/Controllers/ClientesController.cs
+++ ApiParanaBanco/ApiParanaBanco/Controllers/ClientesController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ApiParanaBanco.Model;
+using System.Net;
 
 namespace ApiParanaBanco.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json")]
     public class ClientesController : ControllerBase
     {
         private static List<Cliente> cliente = new();
@@ -13,9 +15,17 @@ namespace ApiParanaBanco.Controllers
         /// <summary>
         /// Obter todos os clientes
         /// </summary>
+        /// <remarks> Exemplo de requisição:
+        /// 
+        /// GET api/Clientes
+        /// </remarks>
         /// <returns>JSON com lista de Clientes cadastrados</returns>
+        /// <response code="200">Retorna a lista contendo os Clientes encontrados.</response>
+        /// <response code="400">Ocorreu um erro ao obter clientes: (Detalhes do erro)</response>
         // GET: api/<ClientesController>
         [HttpGet]
+        [ProducesResponseType(typeof(List<Cliente>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         public IActionResult Get()
         {
             try
@@ -28,13 +38,22 @@ namespace ApiParanaBanco.Controllers
             }
         }
 
+
         /// <summary>
         /// Obter dados do cliente por email
         /// </summary>
         /// <param name="email">email do cliente</param>
-        /// <returns></returns>
+        /// <remarks> Exemplo de requisição:
+        /// 
+        /// GET api/Clientes/email%40dominio.com.br
+        /// </remarks>
+        /// <returns>JSON com os dados do cliente</returns>
+        /// <response code="200">Retorna os dados do cliente com base no email informado</response>
+        /// <response code="400">Ocorreu um erro ao obter o cliente específico: (Detalhes do erro)</response>
         // GET api/<ClientesController>/5
         [HttpGet("{email}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         public IActionResult Get(string email)
         {
             try
@@ -61,8 +80,18 @@ namespace ApiParanaBanco.Controllers
         /// Cadastro de novo Cliente
         /// </summary>
         /// <param name="value">JSON com nome do cliente e Email</param>
-        /// <returns>Status 200 (OK) Cliente Cadastrado ou 400 (BadRequest) algum erro ocorreu</returns>
+        /// <remarks> Exemplo de requisição:
+        /// 
+        /// POST api/Clientes/
+        /// </remarks>
+        /// <returns>Status da requisição</returns>
+        /// <response code="200">Cliente cadastrado com sucesso.</response>
+        /// <response code="400">Erro ao cadastrar cliente: (Detalhes do erro)</response>
+        /// <response code="401">Caso já tenha um email idêntico cadastrado: Já existe um cliente cadastrado com este Email.</response>
         [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
         public IActionResult Post([FromBody] Cliente value)
         {
             try
@@ -74,7 +103,7 @@ namespace ApiParanaBanco.Controllers
                 //verificar se já existe mesmo email já cadastrado
                 if (Clientes.FirstOrDefault(c => c.Email == value.Email) != null)
                 {
-                    return BadRequest("e-mail já cadastrado!");
+                    return Unauthorized("Já existe um cliente cadastrado com este Email.");
                 }
                 //obter último ID
                 value.Id = Clientes.Count > 0 ? Clientes.Max(c => c.Id) + 1 : 1;
@@ -91,11 +120,21 @@ namespace ApiParanaBanco.Controllers
         /// <summary>
         /// Atualizar um cliente cadastrado na base de dados
         /// </summary>
-        /// <param name="id">Id do cliente</param>
+        /// <param name="id">Id do cliente a atualizar</param>
         /// <param name="value">JSON com nome completo e email para atualizar</param>
-        /// <returns></returns>
+        /// <remarks> Exemplo de requisição:
+        /// 
+        /// PUT api/Clientes/
+        /// </remarks>
+        /// <returns>Status da requisição</returns>
+        /// <response code="200">Cliente atualizado com sucesso.</response>
+        /// <response code="400">Ocorreu um erro ao tentar atualizar cliente:  (Detalhes do erro)</response>
+        /// <response code="401">Caso já tenha um email idêntico cadastrado: Já existe um cliente cadastrado com este Email.</response>
         // PUT api/<ClientesController>/5
         [HttpPut("{id}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
         public IActionResult Put(int id, [FromBody] Cliente value)
         {
             try
@@ -108,9 +147,9 @@ namespace ApiParanaBanco.Controllers
                 Cliente? clienteOld = Clientes.FirstOrDefault(c => c.Id == id);
 
                 //verificar se o email que está tentando cadastrar já existe na base de dados
-                if (Clientes.FirstOrDefault(c=>c.Email == value.Email) != null)
+                if (Clientes.FirstOrDefault(c => c.Email == value.Email) != null)
                 {
-                    return BadRequest("Já existe um cliente cadastrado com este Email.");
+                    return Unauthorized("Já existe um cliente cadastrado com este Email.");
                 }
 
                 if (clienteOld != null)
@@ -135,8 +174,17 @@ namespace ApiParanaBanco.Controllers
         /// </summary>
         /// <param name="email">E-mail cadastrado do cliente</param>
         /// <returns></returns>
+        /// <remarks> Exemplo de requisição:
+        /// 
+        /// DELETE api/Clientes/
+        /// </remarks>
+        /// <returns>Status da requisição</returns>
+        /// <response code="200">Cliente removido com sucesso.</response>
+        /// <response code="400">Ocorreu um erro ao tentar deletar cliente: (Detalhes do erro)</response>
         // DELETE api/<ClientesController>/5
         [HttpDelete("{email}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         public IActionResult Delete(string email)
         {
             try
