5ee1a91f914ddecec9e28ca40af92dc3ee5c8a97
Ewerton  Guimarães <ewertonpg@hotmail.com>
2022-07-25 09:39:41 -0300

    correção de validação na criação de novo cliente e ajustes no controller

--- ApiParanaBanco/ApiParanaBanco/Controllers/ClientesController.cs
+++ ApiParanaBanco/ApiParanaBanco/Controllers/ClientesController.cs
@@ -24,16 +24,28 @@ namespace ApiParanaBanco.Controllers
         }
 
         // POST api/<ClientesController>
+        /// <summary>
+        /// Cadastro de novo Cliente
+        /// </summary>
+        /// <param name="value">JSON com nome do cliente e Email</param>
+        /// <returns>Status 200 (OK) Cliente Cadastrado ou 400 (BadRequest) algum erro ocorreu</returns>
         [HttpPost]
         public IActionResult Post([FromBody] Cliente value)
         {
             try
             {
+                //validação de dados
+                value.ValidarEmail(value);
+                value.ValidarNome(value);
+                
+                //verificar se já existe mesmo email já cadastrado
                 if (Clientes.FirstOrDefault(c => c.Email == value.Email) != null)
                 {
                     return BadRequest("e-mail já cadastrado!");
                 }
+                //obter último ID
                 value.Id = Clientes.Count > 0 ? Clientes.Max(c => c.Id) + 1 : 1;
+                //Cadastrar cliente na Lista
                 Clientes.Add(value);
                 return Ok();
             }
--- ApiParanaBanco/ApiParanaBanco/Model/Cliente.cs
+++ ApiParanaBanco/ApiParanaBanco/Model/Cliente.cs
@@ -7,5 +7,24 @@ namespace ApiParanaBanco.Model
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
+
+        public void ValidarNome(Cliente cliente)
+        {
+            if (cliente.Nome.Length < 3)
+            {
+                throw new Exception("Nome inválido!");
+            }
+        }
+
+        public void ValidarEmail(Cliente cliente)
+        {
+            if (!cliente.Email.Contains('@'))
+            {
+                throw new Exception("E-mail inválido!");
+            }
+        }
+
     }
+
+  
 }
