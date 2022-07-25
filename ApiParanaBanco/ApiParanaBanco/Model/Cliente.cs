e7d1245a4a77da527bc8559bf74861491a907d72
Ewerton  Guimarães <ewertonpg@hotmail.com>
2022-07-25 09:17:21 -0300

    criação dos atributos da Model cliente e encapsulamento de dados

--- ApiParanaBanco/ApiParanaBanco/Model/Cliente.cs
+++ ApiParanaBanco/ApiParanaBanco/Model/Cliente.cs
@@ -2,5 +2,36 @@
 {
     public class Cliente
     {
+        public int Id { get; set; }
+        public string Nome
+        {
+            get
+            {
+                return Nome;
+            }
+            set
+            {
+                //validar nome
+                if (value.Length < 3)
+                {
+                    throw new Exception("Nome inválido!");
+                }
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return Email;
+            }
+            //validar email ao cadastrar
+            set
+            {
+                if (!value.Contains('@') || !value.Contains('.'))
+                {
+                    throw new Exception("E-mail inválido!");
+                }
+            }
+        }
     }
 }
